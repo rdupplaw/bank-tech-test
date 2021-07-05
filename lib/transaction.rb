f1fe4af1478df24f5507125213347c726a084c8f
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-05 15:56:22 +0100

    Add error if given both credit and debit

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,7 +4,9 @@
 class Transaction
   attr_reader :date, :credit, :debit
 
-  def initialize(date:, credit:, debit:)
+  def initialize(date:, credit: nil, debit: nil)
+    raise 'Transaction cannot have both credit and debit' if credit && debit
+
     @date = date
     @credit = credit
     @debit = debit
--- spec/model/transaction_spec.rb
+++ spec/model/transaction_spec.rb
@@ -3,17 +3,33 @@
 require 'transaction'
 
 describe Transaction do
-  subject(:transaction) { described_class.new(date: '10-01-2012', credit: 1000, debit: 500) }
+  context 'when given a credit' do
+    subject(:credit_transaction) { described_class.new(date: '10-01-2012', credit: 1000) }
 
-  it 'has a date' do
-    expect(transaction.date).to eq('10-01-2012')
+    it 'has a date' do
+      expect(credit_transaction.date).to eq('10-01-2012')
+    end
+
+    it 'has a credit' do
+      expect(credit_transaction.credit).to eq(1000)
+    end
   end
 
-  it 'has a credit' do
-    expect(transaction.credit).to eq(1000)
+  context 'when given a debit' do
+    subject(:debit_transaction) { described_class.new(date: '03-11-2016', debit: 500) }
+
+    it 'has a date' do
+      expect(debit_transaction.date).to eq('03-11-2016')
+    end
+
+    it 'has a debit' do
+      expect(debit_transaction.debit).to eq(500)
+    end
   end
 
-  it 'has a debit' do
-    expect(transaction.debit).to eq(500)
+  it 'raises an error if given both credit and debit' do
+    expect do
+      described_class.new(date: '13-05-2022', credit: 2000, debit: 9000)
+    end.to raise_error 'Transaction cannot have both credit and debit'
   end
 end
