f772e248d5d87172a53ba423c45c2d750d674efe
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-05 16:35:47 +0100

    Move param validation to private method

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,9 +5,7 @@ class Transaction
   attr_reader :date, :credit, :debit, :balance
 
   def initialize(date:, previous_balance:, credit: nil, debit: nil)
-    raise 'Transaction cannot have both credit and debit' if credit && debit
-
-    raise 'Transaction must have credit or debit' unless credit || debit
+    validate_params(credit, debit)
 
     @date = date
     @credit = credit
@@ -16,6 +14,11 @@ class Transaction
   end
 
   private
+
+  def validate_params(credit, debit)
+    raise 'Transaction cannot have both credit and debit' if credit && debit
+    raise 'Transaction must have credit or debit' unless credit || debit
+  end
 
   def calculate_new_balance(previous_balance, credit, debit)
     return previous_balance + credit if credit
